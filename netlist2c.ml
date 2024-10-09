9c4913953c755b327093d2a6ce59c7ab9596f303
Aghilas Y. Boussaa <aghilas.boussaa@ens.psl.eu>
2024-10-09 16:00:26 +0200

    Cleaned the C transpiler.

--- netlist2c.ml
+++ netlist2c.ml
@@ -1,19 +1,22 @@
 open Netlist_ast
+open Printf
 
 let read_rom = ref ""
 
 let build_mem fd p =
-  let treat (x, e) = match e with
+  let treat (x, e) = fprintf fd "uint64_t %s=0;" x; match e with
       Eram (ads, ws, _, _, _ ,_) ->
-      Printf.fprintf fd "uint64_t ram_%s[%d] = {0}; uint64_t %s = 0; uint64_t wad_%s = 0; uint64_t wda_%s = 0;" x (1 lsl ads) x x x
+      fprintf fd "uint64_t ram_%s[%d]={0};uint64_t wad_%s=0;uint64_t wda_%s=0;"
+        x (1 lsl ads) x x
     | Erom (ads, ws, _) ->
-      Printf.fprintf fd "uint64_t rom_%s[%d] = {0}; uint64_t %s = 0;" x (1 lsl ads) x;
+      fprintf fd "uint64_t rom_%s[%d]={0};" x (1 lsl ads);
       read_rom :=
-        !read_rom ^ Printf.sprintf
-          "f_ = fopen(\"rom/%s\", \"r\"); for (int j_ = 0; (c_ = getc(f_)) != EOF; ++j_)
-rom_%s[j_/%d] |= (c_ == '1') << (%d - (j_ %% %d) - 1); fclose(f_);" x x ws ws ws
-    | Ereg _ -> Printf.fprintf fd "uint64_t %s = 0; uint64_t tmp_%s_ = 0;" x x
-    | _ -> Printf.fprintf fd "uint64_t %s = 0;" x
+        !read_rom ^ sprintf
+          "f_ = fopen(\"rom/%s\",\"r\"); 
+for (int j_=0;(c_=getc(f_))!=EOF;++j_)
+rom_%s[j_/%d]|=(c_=='1')<<(%d-(j_ %% %d)-1);fclose(f_);" x x ws ws ws
+    | Ereg _ -> fprintf fd "uint64_t tmp_%s_=0;" x
+    | _ -> ()
   in
   List.iter treat p
 
@@ -21,27 +24,22 @@ let compile f =
   let p = Netlist.read_file f in
   let p = Scheduler.schedule p in
   let fd = open_out "out.c" in
-  Printf.fprintf fd "#include <stdint.h>\n#include <stdio.h>\n#include <stdlib.h>\n#include <unistd.h>\n";
+  fprintf fd "#include <stdint.h>\n#include <stdio.h>\n#include <stdlib.h>
+#include <unistd.h>\n";
   build_mem fd p.p_eqs;
-  List.iter (Printf.fprintf fd "uint64_t %s;") p.p_inputs;
-  Printf.fprintf fd
-    "int main(){char buf[100]; FILE *f_; int c_; %s for (int i_ = 0; 1; ++i_){printf(\"cycle : %%d\\n\", i_);" !read_rom;
+  List.iter (fprintf fd "uint64_t %s;") p.p_inputs;
+  fprintf fd
+    "int main(){char buf[65];FILE *f_;int c_; %s 
+for (int i_=0;1;++i_){printf(\"cycle : %%d\\n\", i_);" !read_rom;
   let read_input x =
-    Printf.fprintf fd "printf(\"%s? \");fgets(buf, 100, stdin);%s=strtoul(buf, NULL, 2);" x x
+    fprintf fd "printf(\"%s? \");fgets(buf,65,stdin);%s=strtoul(buf,NULL,2);" x x
   in
   List.iter read_input p.p_inputs;
-  let print = function
+  let print = let string_of_bool b = if b then "1" else "0" in function
       Avar s -> s
-    | Aconst (VBit b) -> if b then "1" else "0"
+    | Aconst (VBit b) -> string_of_bool b
     | Aconst (VBitArray a) ->
-      "0b" ^ (Array.fold_right (^) (Array.map (fun b -> if b then "1" else "0") a) "")
-  in
-  let load = function
-      Avar s -> output_string fd s
-    | Aconst (VBit b) -> output_string fd @@ if b then "1" else "0"
-    | Aconst (VBitArray a) ->
-      output_string fd "0b";
-      Array.iter (fun b -> output_string fd @@ if b then "1" else "0") a
+      "0b" ^ (Array.fold_right (^) (Array.map string_of_bool a) "")
   in
   let size = function
     Avar x -> begin
@@ -54,44 +52,40 @@ let compile f =
   in
   let end_loop_ram = ref "" in
   let end_loop_reg = ref "" in
+  let mask a =
+    sprintf "(%s&((1<<%d)-1))" (print a) (size a)
+  in
   let emit_equ (x, e) = match e with
-      Earg a -> Printf.fprintf fd "%s=" x; load a; Printf.fprintf fd ";"
+      Earg a -> fprintf fd "%s=%s;" x (print a)
     | Ereg y ->
-      end_loop_ram := Printf.sprintf "tmp_%s_=%s;" x y ^ !end_loop_ram;
-      end_loop_reg := Printf.sprintf "%s=tmp_%s_;" x x ^ !end_loop_reg
-    | Enot a -> Printf.fprintf fd "%s=~" x; load a; Printf.fprintf fd ";"
-    | Ebinop (Or, a, a') ->
-      Printf.(fprintf fd "%s=" x; load a; fprintf fd "|"; load a'; fprintf fd ";")
-    | Ebinop (And, a, a') ->
-      Printf.(fprintf fd "%s=" x; load a; fprintf fd "&"; load a'; fprintf fd ";")
-    | Ebinop (Xor, a, a') ->
-      Printf.(fprintf fd "%s=" x; load a; fprintf fd "^"; load a'; fprintf fd ";")
-    | Ebinop (Nand, a, a') ->
-      Printf.(fprintf fd "%s=~(" x; load a; fprintf fd "&"; load a'; fprintf fd ");")
+      end_loop_ram := sprintf "tmp_%s_=%s;" x y ^ !end_loop_ram;
+      end_loop_reg := sprintf "%s=tmp_%s_;" x x ^ !end_loop_reg
+    | Enot a -> fprintf fd "%s=~%s;" x (print a)
+    | Ebinop (Or, a, a') -> fprintf fd "%s=%s|%s;" x (print a) (print a')
+    | Ebinop (And, a, a') -> fprintf fd "%s=%s&%s;" x (print a) (print a')
+    | Ebinop (Xor, a, a') -> fprintf fd "%s=%s^%s;" x (print a) (print a')
+    | Ebinop (Nand, a, a') -> fprintf fd "%s=~(%s&%s);" x (print a) (print a')
     | Emux (c, f, t) ->
-      Printf.(fprintf fd "%s=(" x; load c; fprintf fd ") ?(";
-              load t; fprintf fd ") : ("; load f; fprintf fd ");")
+      fprintf fd "%s=(%s)?(%s):(%s);" x (print c) (print t) (print f)
     | Erom (ads, _, a) ->
-      Printf.(fprintf fd "%s=rom_%s[" x x; load a; fprintf fd "&((1<<%d)-1)];" ads)
+      fprintf fd "%s=rom_%s[%s];" x x (mask a)
     | Eram (ads, _, a, wen, wad, wda) ->
       end_loop_ram :=
-        !end_loop_ram ^ Printf.(sprintf "if(wda_%s)ram_%s[wad_%s&((1<<%d)-1)]=%s;"
+        !end_loop_ram ^ (sprintf "if(wda_%s)ram_%s[wad_%s&((1<<%d)-1)]=%s;"
                               x x x ads (print wda)); 
-      Printf.(fprintf fd "%s=ram_%s[" x x; load a; fprintf fd "&((1<<%d)-1)];" ads;
-              fprintf fd "wda_%s=" x; load wen; fprintf fd ";wad_%s=" x;
-              load wad; fprintf fd ";")
+      fprintf fd "%s=ram_%s[%s];" x x (mask a);
+      fprintf fd "wda_%s=%s;wad_%s=%s;" x (print wen) x (print wad)
     | Econcat (a, a') ->
-      Printf.(fprintf fd "%s=(" x; load a; fprintf fd "<<%d" (size a');
-              fprintf fd ")|("; load a'; fprintf fd "&((1<<%d)-1));" (size a'))
+      fprintf fd "%s=(%s<<%d)|(%s);" x (print a) (size a') (mask a')
     | Eselect (i, a) ->
-      Printf.(fprintf fd "%s=((" x; load a; fprintf fd ")>>%d)&1;" (size a - i - 1))
+      fprintf fd "%s=((%s)>>%d)&1;" x (print a) (size a - i - 1)
     | Eslice (i, j, a) ->
-      Printf.(fprintf fd "%s=((" x; load a; fprintf fd ")>>%d)&((1<<%d)-1);" (size a - j - 1) (j - i + 1))
-
+      fprintf fd "%s=((%s)>>%d)&((1<<%d)-1);" x (print a) (size a - j - 1)
+        (j - i + 1)
   in
   List.iter emit_equ p.p_eqs;
   let out x =
-    Printf.fprintf fd "printf(\"%s: %%ld\\n\", %s &((1 << %d) - 1));" x x (size (Avar x))
+    fprintf fd "printf(\"%s: %%ld\\n\",%s);" x (mask (Avar x))
   in
   List.iter out p.p_outputs;
   output_string fd !end_loop_ram;
